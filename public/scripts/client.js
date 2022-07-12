58982fa5fa92c11416f56be4245fe0f23bfdbb32
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-07-12 19:16:40 +0000

    Display empty message error alert

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants. "
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 //Assign tweet timeline container to variable
 const card = document.querySelector('.tweets-timeline');
@@ -43,7 +19,7 @@ const createTweetElement = function(tweet) {
     </header>
     <p>${tweet['content']['text']}</p>
     <footer class="tweet-footer">
-      <span id="days-since-tweet">${tweet['created_at']}</span>
+      <span id="days-since-tweet">${timeago.format(tweet['created_at'])}</span>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
       <i class="fa-solid fa-heart"></i>
@@ -56,7 +32,6 @@ const createTweetElement = function(tweet) {
 
 //Function that displays all the tweets
 const renderTweet = function(tweets) {
-  console.log(tweets)
   for(const tweet of tweets) {
     $(card).append(createTweetElement(tweet));
   }
@@ -76,12 +51,16 @@ loadTweets();
 const newTweet = document.querySelector('form')
 
 $(newTweet).submit(function(event){
-  $.ajax('/tweets', 
-  {
-    method: 'POST',
-    data: $(this).serialize()
-  })
-  // .done(loadTweets())
+  console.log($(this).serialize())
+  if($(this).serialize() === 'text=') {
+    alert("Message can't be empty");
+  } else {
+    $.ajax('/tweets', 
+    {
+      method: 'POST',
+      data: $(this).serialize()
+    })
+  }
   
   event.preventDefault();
 })
